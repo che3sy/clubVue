d62c71d3e191017b525a70bd827d8bf9b90b2433
Cheesy <126740953+che3sy@users.noreply.github.com>
2024-11-02 20:42:07 -0700

    Update useData.js

--- src/components/useData.js
+++ src/components/useData.js
@@ -5,7 +5,7 @@ export async function data() {
 	const people = ref([]);
 
 	const response = await axios.get(
-		"https://sheets.googleapis.com/v4/spreadsheets/1KY4Htn302LepK-wK4jYyYc_4yFFLXm8in1QIjESep8c/values/Sheet1!A1:C18?key=AIzaSyB4s8eDmCCjsIzJyJUNzskhjK_LCssyMyI"
+		"API KEY"
 	);
 
 	const values = response.data.values;
